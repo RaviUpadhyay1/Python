bc2b5c2b03b1286bde54a8f2ac7dc0d97651cb93
Ravi Upadhyay <93428584+RaviUpadhyay1@users.noreply.github.com>
2023-05-19 19:35:38 +0530

    Update and rename greater.py to whether_the_post_contain_ravi.py

---
+++ whether_the_post_contain_ravi.py
@@ -0,0 +1,9 @@
+post = "Hello everyone this is ravi upadhyay a pyhton programmer"
+if ("Ravi" in post):
+    print("Ravi is in post")
+elif ("ravi" in post):
+    print("Ravi is in post")
+elif ("RAVI" in post):
+    print("Ravi is in post")
+else:
+    print("Ravi is not in the post")
